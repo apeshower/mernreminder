4ba0b03de3f5a00c69ae85ef945f768f30ce8f1d
apeshower <89090194+apeshower@users.noreply.github.com>
2022-02-14 16:32:41 +0700

    Update BackButton.tsx

--- components/ui/BackButton.tsx
+++ components/ui/BackButton.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router"
 const BackButton = () => {
     const router = useRouter();
     return (
-        <div className='flex ml-8 mb-4 cursor-pointer' onClick={() => router.back()}>
+        <div className='flex ml-8 mb-4 cursor-pointer' onClick={() => router.push('/')}>
             <div className='w-[24px] h-[24px]'>
                 <Image
                     src={LeftArrow}
